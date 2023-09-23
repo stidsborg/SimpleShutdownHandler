2d37fb19423d5299dc6f9d8c5ee708a0f461d4bd
stidsborg <stidsborg@cleipnir.net>
2023-09-23 17:03:02 +0200

    Implemented simple shutdown handler for console app

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SimpleShutdownHandler;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        WaitForPosixShutdownSignal();
+    }
+
+    private static void WaitForPosixShutdownSignal()
+    {
+        using var reg = PosixSignalRegistration.Create(
+            PosixSignal.SIGINT,
+            handler: context =>
+            {
+                Console.WriteLine($"ShutdownHandler: 'Started after {context.Signal} signal received!'");
+                Thread.Sleep(1000);
+                Console.WriteLine("ShutdownHandler: 'Completed handler'");
+            });
+        
+        Thread.Sleep(-1);
+    }
+
+    private static void AppDomainProcessExit()
+    {
+        AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
+        {
+            Console.WriteLine("Exiting");
+            Thread.Sleep(1000);
+            Console.WriteLine("Completed handler");
+        };
+    }
+}
